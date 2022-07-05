21db2c7cd029688266b36cfb82252e55e899b6e3
Romulo Motta <pukpen@gmail.com>
2022-07-05 09:17:14 -0300

    Custom Listener

--- str-consumer/src/main/java/com/motta/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/motta/strconsumer/listeners/StrConsumerListener.java
@@ -1,5 +1,6 @@
 package com.motta.strconsumer.listeners;
 
+import com.motta.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -8,23 +9,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-0",
-            topics = "str-topic",
-            containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void listener(String message) {
         log.info("Listener ::: Message received {}", message);
     }
 
-    @KafkaListener(groupId = "group-1",
-            topics = "str-topic",
-            containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message) {
         log.info("Log ::: Message received {}", message);
     }
 
-    @KafkaListener(groupId = "group-2",
-            topics = "str-topic",
-            containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-2")
     public void history(String message) {
         log.info("History ::: Message received {}", message);
     }
